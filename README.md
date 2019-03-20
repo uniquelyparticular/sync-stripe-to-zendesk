ef40c6249bf9229724db770fded511066d0f1e70
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-03-20 05:00:03 -0500

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# sync-stripe-to-zendesk
-This example demonstrates how you can sync payment refunds from Stripe into Zendesk Sunshine via Webhook API.
+# @particular./sync-stripe-to-zendesk
+
+> Add a Zendesk Sunshine Evenet when payment refunded in Stripe
+
+Asynchronous microservice that is triggered by [Stripe](https://stripe.com) webhooks to create a Sunshine Event inside of [Zendesk](https://zendesk.com).
+
+Built with [Micro](https://github.com/zeit/micro)! 🤩
+
+## 🛠 Setup
+
+Both a [Zendesk](https://zendesk.com) _and_ [Stripe](https://stripe.com) account are needed for this to function.
+
+Create a `.env` at the project root with the following credentials:
+
+```dosini
+STRIPE_SECRET_KEY=
+ZENDESK_INTEGRATION_EMAIL=
+ZENDESK_INTEGRATION_SECRET=
+```
+
+Find your `STRIPE_SECRET_KEY` within Stripe's [API Settings](https://dashboard.stripe.com/account/apikeys).
+
+TODO: add instructions on ZENDESK keys above
+
+## 📦 Package
+
+Run the following command to build the app
+
+```bash
+yarn install
+```
+
+Start the development server
+
+```bash
+yarn dev
+```
+
+The server will typically start on PORT `3000`, if not, make a note for the next step.
+
+Start ngrok (change ngrok port below from 3000 if yarn dev deployed locally on different port above)
+
+```bash
+ngrok http 3000
+```
+
+Make a note of the https 'URL' ngrok provides.
+
+## ⛽️ Usage
+
+Next head over to the Stripe [Webhook Settings](https://dashboard.stripe.com/account/webhooks) area, add a new webhook with the following details:
+
+| URL to be called    | Webhook version        | Filter event                               |
+| ------------------- | ---------------------- | ------------------------------------------ |
+| _`ngrok URL` above_ | `2018-05-21 (Default)` | 'Select types to send' > `charge.refunded` |
+
+## 🚀 Deploy
+
+You can easily deploy this function to [now](https://now.sh).
---
+++ index.js
@@ -0,0 +1,149 @@
+const { buffer, send } = require('micro')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const moment = require('moment')
+const currency = require('currency.js')
+const fetch = require('node-fetch')
+
+const cors = require('micro-cors')({
+  allowMethods: ['POST'],
+  exposeHeaders: ['stripe-signature'],
+  allowHeaders: [
+    'stripe-signature',
+    'user-agent',
+    'x-forwarded-proto',
+    'X-Requested-With',
+    'Access-Control-Allow-Origin',
+    'X-HTTP-Method-Override',
+    'Content-Type',
+    'Authorization',
+    'Accept'
+  ]
+})
+
+const _toJSON = error => {
+  return !error
+    ? ''
+    : Object.getOwnPropertyNames(error).reduce(
+        (jsonError, key) => {
+          return { ...jsonError, [key]: error[key] }
+        },
+        { type: 'error' }
+      )
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(
+    'Promise unhandledRejection: ',
+    p,
+    ', reason:',
+    JSON.stringify(reason)
+  )
+})
+
+module.exports = cors(async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return send(res, 200, 'ok!')
+  }
+
+  try {
+    const sig = await req.headers['stripe-signature']
+    const body = await buffer(req)
+
+    const {
+      type,
+      data: {
+        object: {
+          id: reference,
+          customer: stripe_id,
+          created,
+          status,
+          refunded,
+          amount: total_paid,
+          amount_refunded: total_refunded,
+          refunds: { total_count: number_refunds },
+          source: {
+            card: { last4: card_last4, brand: card_brand }
+          },
+          metadata: { email, order_id, customer_id, customer_name }
+        },
+        previous_attributes: { amount_refunded: previously_refunded }
+      }
+    } = await stripe.webhooks.constructEvent(
+      body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    )
+
+    if (order_id && type === 'charge.refunded' && status === 'succeeded') {
+      const payload = {
+        profile: {
+          source: 'support',
+          identifiers: {
+            stripe_id: stripe_id,
+            moltin_id: customer_id,
+            email: email
+          }
+        },
+        event: {
+          source: 'stripe',
+          type: 'stripe-payment-refund',
+          description: refunded === true ? 'Order Refunded' : 'Partial Refund',
+          created_at: moment.unix(created),
+          properties: {
+            'Transcation Reference': reference,
+            'Customer Name': customer_name,
+            'Order ID': order_id,
+            'Order Total': currency(total_paid / 100, {
+              formatWithSymbol: true
+            }).format(),
+            'Amount Refunded': currency(
+              (total_refunded - previously_refunded) / 100,
+              { formatWithSymbol: true }
+            ).format(),
+            [`Total Refunds (${number_refunds}) - ${Math.round(
+              (total_refunded / total_paid) * 100
+            )}%`]: currency(total_refunded / 100, {
+              formatWithSymbol: true
+            }).format()
+          }
+        }
+      }
+
+      fetch('https://d3v-uniquelyparticular.zendesk.com/api/cdp/v2/track', {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.ZENDESK_INTEGRATION_EMAIL}/token:${
+              process.env.ZENDESK_INTEGRATION_SECRET
+            }`
+          ).toString('base64')}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(payload)
+      })
+        .then(response => {
+          if (response.ok && response.status < 299) {
+            return send(res, 200, JSON.stringify({ received: true }))
+          } else {
+            return send(res, 500, 'Error')
+          }
+        })
+        .catch(error => {
+          const jsonError = _toJSON(error)
+          return send(
+            res,
+            jsonError.type === 'StripeSignatureVerificationError' ? 401 : 500,
+            jsonError
+          )
+        })
+    }
+  } catch (error) {
+    const jsonError = _toJSON(error)
+    return send(
+      res,
+      jsonError.type === 'StripeSignatureVerificationError' ? 401 : 500,
+      jsonError
+    )
+  }
+})
